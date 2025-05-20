4196cbf5415e0eafdd968a1517fb2919a34a8fea
reinaldodlc <85895621+reinaldodlc@users.noreply.github.com>
2025-05-20 09:11:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = [{"item_name": "Arroz"}, {"item_name": "Feijão"}, {"item_name": "Macarrão"}]
+
+@app.get("/items/")
+async def read_item():
+    return items
